d623ea1c3e7bede25a40cd563a13b78b90dcba73
prabhattopi <topi9864@gmail.com>
2022-05-06 12:47:14 +0530

    ijdifjiodsjfi

---
+++ components/navbar.js
@@ -0,0 +1,24 @@
+function navbar(){
+    return `
+    <h3><a href="index.html">HOme</a></h3>
+    </div>
+    <div id="options">
+        <h3>
+            <a href="jewellery.html">jewellery</a>
+        </h3>
+        <h3>
+            <a href="electronic.html">Electronic</a>
+        </h3>
+        <h3>
+            <a href="login.html">Login</a>
+        </h3>
+        <h3>
+            <a href="signup.html">signup</a>
+        </h3>
+            
+       
+    </div>
+    `
+}
+
+export { navbar } ;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { navbar } from "../components/navbar"
+
+import "../styles/navbar.css"
+console.log("ansdbs",navbar)
+let navbar_div=document.getElementById("navbar")
+navbar_div.innerHTML=navbar()//we got out data in webpage
+
